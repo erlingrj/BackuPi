2df5f049746828fc4e814274b315a8f3c01fb367
Erling Rennemo Jellum <erling.r.jellum@ntnu.no>
2021-01-30 17:09:12 +0100

    uncomment rsync

--- backup-client.sh
+++ backup-client.sh
@@ -9,7 +9,7 @@ PiAddr="bu@10.147.20.85"
 ssh ${PiAddr} "/home/bu/backups.sh ${pcName} newMonth"
 
 # Run rsync backup home folder
-#rsync -avzpH --partial --delete ~/ "${PiAddr}:${backupPath}"
+rsync -avzpH --partial --delete ~/ "${PiAddr}:${backupPath}"
 
 # Make backup and check regarding free space
 ssh ${PiAddr} "/home/bu/backups.sh ${pcName} hardLink"
